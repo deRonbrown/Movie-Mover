0edc0ef9215fb864607fe8d529eca8c47ffc4b8b
DeRon Brown <dmbrown@DeRons-MacBook.local>
2011-05-25 22:54:54 -0400

    Config change. Single update library after moving movies

--- MovieMover.py
+++ MovieMover.py
@@ -5,6 +5,12 @@ import urllib
 from time import sleep, localtime, strftime
 from Config import Config
 
+try:
+	os.remove('./messages.log')
+except OSError as (errno, strerror):
+	error = "Remove of old messages.log failed. {0}: {1}".format(errno, strerror)
+	logErr(error)
+
 logger = logging.getLogger('Movie-Mover')
 hdlr = logging.FileHandler('./messages.log')
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
@@ -25,6 +31,7 @@ def logErr(msg):
 running = True
 while running:
 	log("Checking for movies.")
+	update = False
 	for root, dirs, files in os.walk(Config.DL_DIRECTORY):
 		for file in files:
 			path = os.path.join(root, file)
@@ -45,6 +52,7 @@ while running:
 				try:
 					os.rename(path, newpath)
 					log("Rename successful: '" + path + "' to '" + newpath + "'")
+					update = True
 				except OSError as (errno, strerror):
 					error = "Rename (move) failed. {0}: {1}".format(errno, strerror)
 					logErr(error)
@@ -58,14 +66,16 @@ while running:
 					error = "Download complete notification failed. {0}: {1}".format(errno, strerror)
 					logErr(error)
 				
-				updateRpc = '{"jsonrpc": "2.0", "method": "VideoLibrary.ScanForContent", "id": "1"}'
-				log("Attemping to update XBMC library.")
-				try:
-					respdata = urllib.urlopen(Config.JSON_RPC_URL, updateRpc).read()
-					log("Update of XBMC library successful.")
-				except IOError as (errno, strerror):
-					error = "Update of XBMC library failed. {0}: {1}".format(errno, strerror)
-					logErr(error)
+	if (update):
+		updateLibrary = Config.XBMC_URL + "/xbmcCmds/xbmcHttp?command=ExecBuiltIn(UpdateLibrary(video," + Config.MOVIE_DESTINATION + "))"
+		log("Attemping to update XBMC library.")
+		try:
+			respdata = urllib.urlopen(updateLibrary)
+			log("Update of XBMC library successful.")
+		except IOError as (errno, strerror):
+			error = "Update of XBMC library failed. {0}: {1}".format(errno, strerror)
+			logErr(error)
+		update = False
 	
 	for root, dirs, files in os.walk(Config.DL_DIRECTORY):
 		for dir in dirs:
